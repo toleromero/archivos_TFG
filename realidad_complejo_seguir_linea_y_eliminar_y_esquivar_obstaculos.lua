ef3b8a3e341af0f443c97dc9a096fab565290ca3
toleromero <31548638+toleromero@users.noreply.github.com>
2017-09-04 02:55:12 +0200

    Rename realidad_complejo_seguir_linea_y_eliminar_obstaculos_y_esquivar_obstaculos.lua to realidad_complejo_seguir_linea_y_eliminar_y_esquivar_obstaculos.lua

--- realidad_complejo_seguir_linea_y_eliminar_y_esquivar_obstaculos.lua
+++ realidad_complejo_seguir_linea_y_eliminar_y_esquivar_obstaculos.lua
@@ -1,19 +1,19 @@
 velLeft=math.pi/4 -- Velocidad para la rueda izquierda
 velRight=math.pi/4 -- Velocidad para la rueda derecha
-s_ant_izq=1 -- Fuera de la línea
-s_ant_der=1 -- Fuera de la línea
+s_ant_izq=1 -- Fuera de la lÃ­nea
+s_ant_der=1 -- Fuera de la lÃ­nea
 giro=0 -- Derecha
 pinzas=1 -- Brazos arriba
 simExtK3_setGripperGap(170) -- Dedos abiertos
 simExtK3_setArmPosition(300) -- Brazos arriba para que los sensores tengan visibilidad
-esquivar=0 -- Control de esquivación de objetos grandes
-mov=1 -- Contador de número de movimientos para evitar obstáculo grande
+esquivar=0 -- Control de esquivaciÃ³n de objetos grandes
+mov=1 -- Contador de nÃºmero de movimientos para evitar obstÃ¡culo grande
 
 while (1) do
 	
 --LINEA------------------------------------------------------------------------------------------------------------	
 
-	-- Todo lo que sea inferior o igual a 0'95 será considerado como negro (0)
+	-- Todo lo que sea inferior o igual a 0'95 serÃ¡ considerado como negro (0)
 	-- para ambos sensores
 	s_act_izq=simExtK3_getLineSensor(1)
 	if (s_act_izq<=0.95) then
@@ -29,22 +29,22 @@ while (1) do
 		s_act_der=1
 	end	
 
-	-- Si actualmente está en suelo negro y los brazos están arriba, sigue recto
+	-- Si actualmente estÃ¡ en suelo negro y los brazos estÃ¡n arriba, sigue recto
 	if(s_act_izq==0)and(s_act_der==0) and (pinzas==1) then
 		simExtK3_setVelocity(velLeft,velRight)
 		-- Recarga las posiciones anteriores, que son las actuales para tener un 
-		-- historial de dónde ha estado la última vez y así poder realizar los
+		-- historial de dÃ³nde ha estado la Ãºltima vez y asÃ­ poder realizar los
 		-- giros pertinentes
 		s_ant_izq=s_act_izq
 		s_ant_der=s_act_der
 	    
 		-- En caso de que sea necesario reiniciar el contador. Siempre que mov=1,
-        -- significa que no tiene que evitar ningún obstáculo grande	
+        -- significa que no tiene que evitar ningÃºn obstÃ¡culo grande	
 		mov=1
 	end
 
-	-- Si se sale por la izquierda, tiene las pinzas arriba y no está realizando 
-	-- movimientos de esquivar, gira a la derecha 15º
+	-- Si se sale por la izquierda, tiene las pinzas arriba y no estÃ¡ realizando 
+	-- movimientos de esquivar, gira a la derecha 15Âº
 	if(s_act_izq==1 and s_act_der==0) and (mov==1) and (pinzas==1) then
 		simExtK3_setVelocity(velLeft/2,-velRight/2)
 		simWait((4/3)/9)
@@ -56,11 +56,11 @@ while (1) do
 	end
 
 		
-	-- Si se sale por la derecha, tiene las pinzas arriba y no está realizando 
-	-- movimientos de esquivar, gira a la izquierda 15º
+	-- Si se sale por la derecha, tiene las pinzas arriba y no estÃ¡ realizando 
+	-- movimientos de esquivar, gira a la izquierda 15Âº
 	if(s_act_izq==0 and s_act_der==1) and (mov==1) and (pinzas==1) then
 		-- si me salgo por la derecha
-		-- giro a la izquierda 15º
+		-- giro a la izquierda 15Âº
 		simExtK3_setVelocity(-velLeft/2,velRight/2)
 		simWait((4/3)/9)
 		simWait(1/15)
@@ -70,9 +70,9 @@ while (1) do
 	end
 
 	-- Si se sale por los dos lados a la vez y anteriormente estuvo sobre la 
-	-- línea, deberá hacer unos giros más bruscos
+	-- lÃ­nea, deberÃ¡ hacer unos giros mÃ¡s bruscos
 	if(s_act_izq==1 and s_act_der==1) and (mov==1) and (pinzas==1) then
-		-- Si el último giro fue a la derecha, gira 45º a la izquierda	
+		-- Si el Ãºltimo giro fue a la derecha, gira 45Âº a la izquierda	
 		if (giro==0) then
 			simExtK3_setVelocity(-velLeft/2,velRight/2)
 			simWait((4/3)/2)
@@ -80,16 +80,16 @@ while (1) do
 		 
 			-- Al girar a la izquierda, es como si se hubiese salido por la derecha,
 			-- entonces para que siga el giro a izquierda, se le establecen estos 
-			-- parámetros para que entre en la condición de giro a izquierda y siga
-			-- girando en esa misma dirección
+			-- parÃ¡metros para que entre en la condiciÃ³n de giro a izquierda y siga
+			-- girando en esa misma direcciÃ³n
 			s_act_izq=0
 			s_act_der=1
 		
-			-- Así se obliga a que vuelva a entrar aqui para seguir girando a la izquierda
-			-- ya que los giros de 15º en ocasiones son insuficientes para salir del bloqueo
+			-- AsÃ­ se obliga a que vuelva a entrar aqui para seguir girando a la izquierda
+			-- ya que los giros de 15Âº en ocasiones son insuficientes para salir del bloqueo
 			giro=0
 		
-		-- Si el último giro fue a la izquierda, gira 45º a la derecha
+		-- Si el Ãºltimo giro fue a la izquierda, gira 45Âº a la derecha
 		else
 			simExtK3_setVelocity(velLeft/2,-velRight/2)
 			simWait((4/3)/2)
@@ -114,11 +114,11 @@ while (1) do
 	sensor5=simExtK3_getInfrared(5) -- sensor diagonal derecho (vista superior)
 	sensor6=simExtK3_getInfrared(6) -- sensor lateral derecho (vista superior)
 	
-	-- Contador para saber cuántos sensores están detectando
+	-- Contador para saber cuÃ¡ntos sensores estÃ¡n detectando
 	numero_sensores=0
 
 	-- El contador se va incrementando si alguno de los 2 frontales o los 2 diagonales
-	-- realiza alguna detección
+	-- realiza alguna detecciÃ³n
 	if (sensor2<0.1) then
 		numero_sensores= numero_sensores+1
 	end
@@ -141,8 +141,8 @@ while (1) do
 
 
 --COGER------------------------------------------------------------------------------------------------------------	
-	-- Si el contador se sensores es menor o igual a 2, significa que el obstáculo es pequeño, por tanto, hay que cogerlo
-	-- Tambien hay que comprobar si no se ha realizado ningún movimiento de esquivar o de coger
+	-- Si el contador se sensores es menor o igual a 2, significa que el obstÃ¡culo es pequeÃ±o, por tanto, hay que cogerlo
+	-- Tambien hay que comprobar si no se ha realizado ningÃºn movimiento de esquivar o de coger
 	if (numero_sensores<=2) and (s_act_izq==0 and s_act_der==0) 
 		and (((sensor3<0.04 or sensor4<0.04 or sensor2<0.04 or sensor5<0.04) and (mov==1)) or (pinzas==0)) then
 
@@ -156,7 +156,7 @@ while (1) do
 			
 			simExtK3_setVelocity(velLeft,velRight) -- avanza
 
-			-- Indica con la variable booleana que las pinzas están bajadas
+			-- Indica con la variable booleana que las pinzas estÃ¡n bajadas
 			pinzas=0
 		end
 
@@ -227,7 +227,7 @@ while (1) do
 		end
 
 
-		-- Si detecta algo solo con el diagonal izquierdo, gira más brusco hacia la izquierda	
+		-- Si detecta algo solo con el diagonal izquierdo, gira mÃ¡s brusco hacia la izquierda	
 		if (sensor2<=0.04) and (sensor3>=0.12 and sensor4>=0.12 and sensor5>=0.12 and sensor1>=0.12) and (pinzas==1) then    
 			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro a la izquierda 45 grados
 			simWait((4/3)/2)
@@ -244,7 +244,7 @@ while (1) do
 		end
 
 		
-		-- Si detecta algo solo con el diagonal derecho, gira más brusco hacia la derecha	
+		-- Si detecta algo solo con el diagonal derecho, gira mÃ¡s brusco hacia la derecha	
 		if (sensor2<=0.04) and (sensor3>=0.12 and sensor4>=0.12 and sensor5>=0.12 and sensor1>=0.12) and (pinzas==1) then    
 			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro a la derecha 45 grados
 			simWait((4/3)/2)
@@ -264,7 +264,7 @@ while (1) do
 		pinza_derecha=simExtK3_getGripperProxSensor(1)
 		pinza_izquierda=simExtK3_getGripperProxSensor(0)
 		
-		-- Si las pinzas detectan algún objeto y están bajadas		
+		-- Si las pinzas detectan algÃºn objeto y estÃ¡n bajadas		
 		if (pinza_izquierda>200 or pinza_derecha>200) and (pinzas==0) then
 			-- Stop para que pare a coger el objeto
 			simExtK3_setVelocity(0,0)
@@ -278,7 +278,7 @@ while (1) do
 			simWait(2)
 			simWait(1/15)
 
-			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro de 90º a la derecha
+			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro de 90Âº a la derecha
 			simWait(4/3)
 			
 			-- Stop para que pare a dejar el objeto
@@ -296,7 +296,7 @@ while (1) do
 			simWait(2)
 			simWait(1/15)
 
-			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro de 90º a la izquierda
+			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro de 90Âº a la izquierda
 			simWait(4/3)	
 
 			simExtK3_setVelocity(velLeft,velRight) -- avance
@@ -315,9 +315,9 @@ while (1) do
 	
 --ESQUIVAR------------------------------------------------------------------------------------------------------------		
 	
-	-- Si el contador se sensores es mayor que 2, significa que el obstáculo es grande, por tanto, hay que esquivarlo
+	-- Si el contador se sensores es mayor que 2, significa que el obstÃ¡culo es grande, por tanto, hay que esquivarlo
 	-- Hay que comprobar si los sensores diagonales detectan algo y si los sensores lateral y diagonal izquierdo detectan 
-	-- algo mientras que los frontales no detecten nada (para cuando esté realizando los movimientos de esquivar)
+	-- algo mientras que los frontales no detecten nada (para cuando estÃ© realizando los movimientos de esquivar)
 	if (((sensor3<0.04) and (sensor4<0.04) and (((sensor2>0.04) or (sensor5>0.04)) and ((sensor2<0.15) or (sensor5<0.15)))) 
 		or ((sensor3>0.1) and (sensor4>0.1) and ((sensor1<0.05) or (sensor0<0.1))) 
 		and (mov>=1) and (pinzas==1) and (numero_sensores>2)) 
@@ -325,10 +325,10 @@ while (1) do
 	
 		-- Si detecta algo por los sensores frontales
 		if (sensor3<0.04 and sensor4<0.04) and (mov==1) then
-			-- Se incrementa el contador de movimientos porque va a realizar un giro de 90º hacia la
-            -- derecha para evitar el obstáculo grande
+			-- Se incrementa el contador de movimientos porque va a realizar un giro de 90Âº hacia la
+            -- derecha para evitar el obstÃ¡culo grande
 			mov=2
-			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro 90º a la derecha
+			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro 90Âº a la derecha
 			simWait(4/3)
 			giro=0 -- porque ha girado a derecha
 			simWait(1/15)
@@ -342,8 +342,8 @@ while (1) do
 		end
 
 
-		-- Cuando todos los sensores del lateral izquierdo se alejen demasiado del obstáculo, deberán
-		-- volver a acercarse muy muy ligeramente para no perder la detección
+		-- Cuando todos los sensores del lateral izquierdo se alejen demasiado del obstÃ¡culo, deberÃ¡n
+		-- volver a acercarse muy muy ligeramente para no perder la detecciÃ³n
 		if (sensor0>0.06 and sensor1>0.1 and sensor2>0.1) and (mov>=2) then
 			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro a la izquierda
 			simWait(4/3/2/3/2/2)
@@ -352,12 +352,12 @@ while (1) do
 
 
 		-- Cuando todos los sensores del lateral izquierdo dejen de detectar algo, 
-		-- entonces deberá hacer un giro de 90º hacia la izquierda. Hay que comprobar 
-		-- también que esté en la fase 2 del movimiento
+		-- entonces deberÃ¡ hacer un giro de 90Âº hacia la izquierda. Hay que comprobar 
+		-- tambiÃ©n que estÃ© en la fase 2 del movimiento
 		if(sensor1>0.15) and (sensor0>0.15) and (mov==2) then
             -- Se incrementa el contador de movimientos
 			mov=3
-			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro 90º a la izquierda
+			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro 90Âº a la izquierda
 			simWait(4/3)
 			giro=1 -- porque ha girado a izquierda
 			simWait(1/15)
@@ -368,14 +368,14 @@ while (1) do
 
 
 		-- Se aumentan un poquito las distancias permitidas por los errores de giro que va llevando desde el principio
-		-- Cuando los 3 sensores del lateral izquierdo no detecten el obstáculo a una distancia relativamente cerca,
-		-- significa que está lo suficientemente lejos como para poder girar 90º sin molestarle el objeto. Hay que 
-		-- comprobar también que esté en la fase 3 del movimiento		
+		-- Cuando los 3 sensores del lateral izquierdo no detecten el obstÃ¡culo a una distancia relativamente cerca,
+		-- significa que estÃ¡ lo suficientemente lejos como para poder girar 90Âº sin molestarle el objeto. Hay que 
+		-- comprobar tambiÃ©n que estÃ© en la fase 3 del movimiento		
 		if(sensor2>0.1 and sensor1>0.15 and sensor0>0.05) and (mov==3) then
-			-- Como es el último movimiento de esquive, se reinicia el contador
+			-- Como es el Ãºltimo movimiento de esquive, se reinicia el contador
 			mov=0
 			esquivar=0
-			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro 90º a la izquierda
+			simExtK3_setVelocity(-velLeft/2,velRight/2) -- giro 90Âº a la izquierda
 			simWait(4/3)
 			giro=1 -- porque ha girado a izquierda
 			simWait(1/15)
@@ -385,7 +385,7 @@ while (1) do
 		end
 
 
-		-- Una vez que se ha terminado de evitar el obstáculo, se vuelve a realizar una lectura de los valores del suelo
+		-- Una vez que se ha terminado de evitar el obstÃ¡culo, se vuelve a realizar una lectura de los valores del suelo
 		s_act_izq=simExtK3_getLineSensor(1)
 		if (s_act_izq<=0.94) then
 			s_act_izq=0
@@ -400,12 +400,12 @@ while (1) do
 			s_act_der=1
 		end
 
-		-- También hay que tener en cuenta la posibilidad de que un objeto grande se encuentre en una esquina de la línea,
-		-- en este caso, no habría que realizar los 3 movimientos comentados anteriormente y con estas líneas se soluciona
+		-- TambiÃ©n hay que tener en cuenta la posibilidad de que un objeto grande se encuentre en una esquina de la lÃ­nea,
+		-- en este caso, no habrÃ­a que realizar los 3 movimientos comentados anteriormente y con estas lÃ­neas se soluciona
 		if (s_act_izq==0) and (s_act_der==0) and (mov>1) then
 			mov=0
 			esquivar=0
-			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro 15º a la derecha
+			simExtK3_setVelocity(velLeft/2,-velRight/2) -- giro 15Âº a la derecha
 			simWait(4/3/2/3)		
 		end
 
